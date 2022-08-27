9a88f831f2639c259feff641b26c56028f2c889c
MC <79386814+204g1a0584@users.noreply.github.com>
2022-08-28 04:13:29 +0530

    created new function welcome
    defined the new function and also called it in the program which prints a message to welcome the user

--- oslab/sample.c
+++ oslab/sample.c
@@ -6,10 +6,16 @@ Name : PALLAVI
 //source code
 
 #include<stdio.h>
+//function defintion
+void welcome(){
+  printf("Welcome to the Page");
+}
 void main()
 {
   printf("HELLO PALLAVI");
-  printf("Hope you are doing good")
+  printf("Hope you are doing good");
   
+//function call
+  welcome();
   
 }
