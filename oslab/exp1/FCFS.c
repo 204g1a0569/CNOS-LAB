94fb60d91d1be29ef769506cfc208683a2833692
204g1a0569 <100609745+204g1a0569@users.noreply.github.com>
2022-09-16 15:54:07 +0530

    FCFS.c file is created

---
+++ oslab/exp1/FCFS.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: Pallavi.R
+   Roll No: 204G1A0569
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
